43be01a881da22d59437e8cc5a0d501ae60ebd24
belopot <belopot@protonmail.com>
2020-03-26 17:21:42 +0300

    Updates

---
+++ precache-manifest.e5756e8c7d7810a64171431577b98ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80824626ba59266b20188a69387e665",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1416aad2b6274114238",
+    "url": "/static/css/main.96ad13a2.chunk.css"
+  },
+  {
+    "revision": "0d0faa946db141593ccf",
+    "url": "/static/js/2.42b05524.chunk.js"
+  },
+  {
+    "revision": "42ef8554d6941ac90f9a85e5eaff4245",
+    "url": "/static/js/2.42b05524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1416aad2b6274114238",
+    "url": "/static/js/main.05e2dd54.chunk.js"
+  },
+  {
+    "revision": "6de46e96d3f9a8f4d840",
+    "url": "/static/js/runtime-main.3db1c803.js"
+  }
+]);
---
+++ swiperctrl.js
@@ -0,0 +1,55 @@
+var swiper = document.getElementById("swiper-root")
+swiper.innerHTML = "";
+
+
+var data = [
+    { name: "AKM", thumbnail: "assets/thumbnails/akm.jpg", model: "assets/models/akm/scene.gltf" },
+    { name: "DSHK", thumbnail: "assets/thumbnails/dshk.jpg", model: "assets/models/dshk/scene.gltf" },
+    { name: "FAL", thumbnail: "assets/thumbnails/fal.jpg", model: "assets/models/fal/scene.gltf" },
+    { name: "GALIL", thumbnail: "assets/thumbnails/galil.jpg", model: "assets/models/galil/scene.gltf" },
+    { name: "HK21", thumbnail: "assets/thumbnails/hk21.jpg", model: "assets/models/hk21/scene.gltf" },
+    { name: "LG5", thumbnail: "assets/thumbnails/lg5.jpg", model: "assets/models/lg5/scene.gltf" },
+    { name: "Multi Rocket Launcher", thumbnail: "assets/thumbnails/multi_rocket_launcher.jpg", model: "assets/models/multi_rocket_launcher/scene.gltf" },
+    { name: "NORINCO", thumbnail: "assets/thumbnails/norinco.jpg", model: "assets/models/norinco/scene.gltf" },
+    { name: "PKM", thumbnail: "assets/thumbnails/pkm.jpg", model: "assets/models/pkm/scene.gltf" },
+    { name: "QBZ", thumbnail: "assets/thumbnails/qbz.jpg", model: "assets/models/qbz/scene.gltf" },
+    { name: "QLZ", thumbnail: "assets/thumbnails/qlz.jpg", model: "assets/models/qlz/scene.gltf" },
+    { name: "RECOILLESS", thumbnail: "assets/thumbnails/recoilless.jpg", model: "assets/models/recoilless/scene.gltf" },
+    { name: "RPG", thumbnail: "assets/thumbnails/rpg.jpg", model: "assets/models/rpg/scene.gltf" },
+    { name: "SMG", thumbnail: "assets/thumbnails/smg.jpg", model: "assets/models/smg/scene.gltf" },
+    { name: "SPG", thumbnail: "assets/thumbnails/spg.jpg", model: "assets/models/spg/scene.gltf" },
+    { name: "SVD", thumbnail: "assets/thumbnails/svd.jpg", model: "assets/models/svd/scene.gltf" },
+    { name: "AKM56", thumbnail: "assets/thumbnails/type56.jpg", model: "assets/models/type56/scene.gltf" }
+]
+
+for (var i = 0; i < data.length; i++) {
+    var slide = document.createElement("div");
+    slide.classList.add("swiper-slide", "tooltip")
+    slide.style.backgroundImage = `url(./` + data[i].thumbnail + `)`;
+
+    slide.modelURL = data[i].model;
+
+    slide.addEventListener('click', function(event){
+        window.mainScene.loadGLTF(this.modelURL);
+    })
+
+    //Tooltip
+    var tooltip = document.createElement("span");
+    tooltip.classList.add("tooltiptext");
+    tooltip.innerText = data[i].name;
+
+    slide.appendChild(tooltip);
+
+    swiper.appendChild(slide);
+
+}
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 5,
+    slidesPerView: window.innerWidth / (80 * 3),
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+
+
