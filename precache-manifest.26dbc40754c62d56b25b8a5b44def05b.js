46edee57b1e12dccdd2b5a5b9be51be9ca58e3f2
belopot <belopot@protonmail.com>
2020-03-26 20:38:14 +0300

    Updates

---
+++ precache-manifest.26dbc40754c62d56b25b8a5b44def05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1442fcd04aedaf34927b7b645f8071a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af113427be741d0d7f00",
+    "url": "/static/css/main.96ad13a2.chunk.css"
+  },
+  {
+    "revision": "710abe4e0a6d7aaf3ed2",
+    "url": "/static/js/2.809ab8ed.chunk.js"
+  },
+  {
+    "revision": "42ef8554d6941ac90f9a85e5eaff4245",
+    "url": "/static/js/2.809ab8ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af113427be741d0d7f00",
+    "url": "/static/js/main.73941fd8.chunk.js"
+  },
+  {
+    "revision": "7acc2548db05a05f999f",
+    "url": "/static/js/runtime-main.0fcd1274.js"
+  }
+]);
