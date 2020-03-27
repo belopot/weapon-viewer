aaec3b63c2bb34547238fff7339a8d8d5ea1f664
belopot <belopot@protonmail.com>
2020-03-27 21:02:17 +0300

    Updates

--- swiperctrl.js
+++ swiperctrl.js
@@ -3,23 +3,23 @@ swiper.innerHTML = "";
 
 
 var data = [
-    { name: "AKM", thumbnail: "assets/thumbnails/akm.jpg", model: "/assets/models/akm/scene.gltf" },
-    { name: "DSHK", thumbnail: "assets/thumbnails/dshk.jpg", model: "/assets/models/dshk/scene.gltf" },
-    { name: "FAL", thumbnail: "assets/thumbnails/fal.jpg", model: "/assets/models/fal/scene.gltf" },
-    { name: "GALIL", thumbnail: "assets/thumbnails/galil.jpg", model: "/assets/models/galil/scene.gltf" },
-    { name: "HK21", thumbnail: "assets/thumbnails/hk21.jpg", model: "/assets/models/hk21/scene.gltf" },
-    { name: "LG5", thumbnail: "assets/thumbnails/lg5.jpg", model: "/assets/models/lg5/scene.gltf" },
-    { name: "Multi Rocket Launcher", thumbnail: "assets/thumbnails/multi_rocket_launcher.jpg", model: "/assets/models/multi_rocket_launcher/scene.gltf" },
-    { name: "NORINCO", thumbnail: "assets/thumbnails/norinco.jpg", model: "/assets/models/norinco/scene.gltf" },
-    { name: "PKM", thumbnail: "assets/thumbnails/pkm.jpg", model: "/assets/models/pkm/scene.gltf" },
-    { name: "QBZ", thumbnail: "assets/thumbnails/qbz.jpg", model: "/assets/models/qbz/scene.gltf" },
-    { name: "QLZ", thumbnail: "assets/thumbnails/qlz.jpg", model: "/assets/models/qlz/scene.gltf" },
-    { name: "RECOILLESS", thumbnail: "assets/thumbnails/recoilless.jpg", model: "/assets/models/recoilless/scene.gltf" },
-    { name: "RPG", thumbnail: "assets/thumbnails/rpg.jpg", model: "/assets/models/rpg/scene.gltf" },
-    { name: "SMG", thumbnail: "assets/thumbnails/smg.jpg", model: "/assets/models/smg/scene.gltf" },
-    { name: "SPG", thumbnail: "assets/thumbnails/spg.jpg", model: "/assets/models/spg/scene.gltf" },
-    { name: "SVD", thumbnail: "assets/thumbnails/svd.jpg", model: "/assets/models/svd/scene.gltf" },
-    { name: "AKM56", thumbnail: "assets/thumbnails/type56.jpg", model: "/assets/models/type56/scene.gltf" }
+    { name: "AKM", thumbnail: "./assets/thumbnails/akm.jpg", model: "https://raw.githubusercontent.com/belopot/weapon-viewer/gh-pages/assets/models/akm/scene.gltf" },
+    { name: "DSHK", thumbnail: "./assets/thumbnails/dshk.jpg", model: "./assets/models/dshk/scene.gltf" },
+    { name: "FAL", thumbnail: "./assets/thumbnails/fal.jpg", model: "./assets/models/fal/scene.gltf" },
+    { name: "GALIL", thumbnail: "./assets/thumbnails/galil.jpg", model: "./assets/models/galil/scene.gltf" },
+    { name: "HK21", thumbnail: "./assets/thumbnails/hk21.jpg", model: "./assets/models/hk21/scene.gltf" },
+    { name: "LG5", thumbnail: "./assets/thumbnails/lg5.jpg", model: "./assets/models/lg5/scene.gltf" },
+    { name: "Multi Rocket Launcher", thumbnail: "./assets/thumbnails/multi_rocket_launcher.jpg", model: "./assets/models/multi_rocket_launcher/scene.gltf" },
+    { name: "NORINCO", thumbnail: "./assets/thumbnails/norinco.jpg", model: "./assets/models/norinco/scene.gltf" },
+    { name: "PKM", thumbnail: "./assets/thumbnails/pkm.jpg", model: "./assets/models/pkm/scene.gltf" },
+    { name: "QBZ", thumbnail: "./assets/thumbnails/qbz.jpg", model: "./assets/models/qbz/scene.gltf" },
+    { name: "QLZ", thumbnail: "./assets/thumbnails/qlz.jpg", model: "./assets/models/qlz/scene.gltf" },
+    { name: "RECOILLESS", thumbnail: "./assets/thumbnails/recoilless.jpg", model: "./assets/models/recoilless/scene.gltf" },
+    { name: "RPG", thumbnail: "./assets/thumbnails/rpg.jpg", model: "./assets/models/rpg/scene.gltf" },
+    { name: "SMG", thumbnail: "./assets/thumbnails/smg.jpg", model: "./assets/models/smg/scene.gltf" },
+    { name: "SPG", thumbnail: "./assets/thumbnails/spg.jpg", model: "./assets/models/spg/scene.gltf" },
+    { name: "SVD", thumbnail: "./assets/thumbnails/svd.jpg", model: "./assets/models/svd/scene.gltf" },
+    { name: "AKM56", thumbnail: "./assets/thumbnails/type56.jpg", model: "./assets/models/type56/scene.gltf" }
 ]
 
 for (var i = 0; i < data.length; i++) {
